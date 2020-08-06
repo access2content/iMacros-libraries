a6cb1e20f3de324a85c359847d36919b7fb975e6
access2content <access2content@gmail.com>
2020-08-06 21:01:41 +0530

    Fresh code to set folder and file and others
    New code for taking file input from the user.
    Added JSDoc to entire codebase.
    Added a string prototype method to add <SP> to strings

--- File/File.js
+++ File/File.js
@@ -1,257 +1,90 @@
-var File = function(folder,file)
+/**
+ * Working with files in iMacros
+ * @constructor
+ * @param {string} folder  - The folder that contains the file
+ * @param {string} file - Name of the file with extension
+ */
+
+var IFile = function(folder,file)
 {
-        this.folder=folder;
-        this.file=file;
-        this.currentRow=null;
-        this.headers=[];
-        this.values=[];
-        this.totalColumns=null;
-        this.temp;
-        var code;
+    /**@type {string} Complete Path of a folder. Set the {{!DATASOURCE}} by default*/
+    this.folder=folder?folder.replace(/ /g,"<SP>"):"{{!DATASOURCE}}";
 
-        this.install = function()
-        {
-          var aFile = Components.classes["@mozilla.org/file/local;1"]
-          .createInstance(Components.interfaces.nsILocalFile);
-          aFile.initWithPath(this.folder+"\\"+this.file);
-          iimPlayCode("SET !EXTRACT {{!FOLDER_DATASOURCE}}");
-          var folder=iimGetLastExtract().replace("\\iMacros\\Datasources","\\iMacros\\Macros");
-          var aDir = Components.classes["@mozilla.org/file/local;1"]
-            .createInstance(Components.interfaces.nsILocalFile);  
-          aDir.initWithPath(folder);
-          aFile.moveTo(aDir,null);
-        };
+    /**@type {string} Name of the file with extension*/
+    this.file=file?file.replace(/ /g,"<SP>"):"";
+
+    /**@type {string} Path separator. \ for windows and / for other OS*/
+    this.separator = window.navigator.oscpu.indexOf('Window')>=0?'\\':'/';
+
+    /**@type {number} Variable that indicates the row number to be read in the file*/
+    this.currentRow=1;
+
+    /**New String Prototype function to add <SP> to strings */
+    String.prototype.addSP = new Function('return this.replace(/ /g,"<SP>");');
+
+
+
+
+    /**Sets the path of the folder replacing spaces with <SP>
+     * @param {string} folder - Full path of the folder 
+    */
+    this.setFolder = (folder)=>{this.folder=folder.addSP();};
+
+
+    /**Sets the name of the file replacing spaces with <SP>
+     * @param {string} file - Name of the file with extension 
+    */
+    this.setFile = (file)=>{this.file=file.addSP();};
+
+
+    /**Sets the folder and file variables replacing spaces with <SP> 
+     * @param {string} path - Full path of the file including file name and extension
+    */
+    this.setPath = (path)=>{
+        var breakpoint=path.lastIndexOf(this.separator);
+        this.folder = path.substring(0,breakpoint).addSP();
+        this.file = path.substring(breakpoint+1).addSP();        
+    };
+
+
+    /**Select a folder from a folder browser 
+     * @param {string} message - (Optional) The message to display to the user
+     * @returns {string|boolean} The path of the folder if uploaded. If not, returns false
+    */
+    this.selectFolder = (message)=>{
+        message = message?message:"Please select a folder";
         
-        this.upload = function(message,hasHeaders)
+        var upload=imns.Dialogs.browseForFolder(message);
+        if(upload)
         {
-                var upload=imns.Dialogs.browseForFileOpen(message);
-                if(upload)
-                {
-                var breakpoint=upload.path.lastIndexOf("\\");
-                this.currentRow=0;
-                this.folder=upload.path.substring(0,breakpoint).replace(/ /g,"<SP>");
-                this.file=upload.path.substring(breakpoint+1,upload.path.length).replace(/ /g,"<SP>");
-                this.readTotalColumns();
-                
-                if(hasHeaders)
-                {
-                        this.read();
-                        this.headers=this.values;
-                        this.values=[];
-                }
-                
-                return true;
-                }
+            this.folder=upload.path.addSP();
+            return upload.path;
+        }
         return false;
-        };
-        
-        this.selectFolder = function(message)
-        {
-                        var upload=imns.Dialogs.browseForFolder(message);
-                        this.folder=upload.path.replace(/ /g,"<SP>");
-          
-                        if(upload)
-                                return true;
-                        return false;
-        };
-  
-        this.readTotalColumns = function()
-        {
-                var start=1;
-                var end=100;
-                var mid=0;
-                
-                while(start!=end)
-                {
-                        mid = Math.ceil((start+end)/2);
-                        code="CODE:";
-                        code+="SET !DATASOURCE "+this.folder+"\\"+this.file+"\n";
-                        code+="SET !DATASOURCE_LINE 1\n";
-                        code+="SET !EXTRACT {{!COL"+mid+"}}\n";
-                        
-                        if(iimPlay(code)==-953)
-                                end=mid-1;
-                        else
-                                start=mid;
-                }
+    };
 
-                iimPlayCode("WAIT SECONDS=0");
-                
-                code=null;
-                this.totalColumns=start;
-        };
+    
+    /**Select a file from a file browser 
+     * @param {string} message - (Optional) The message to display to the user
+     * @param {boolean} hasHeaders - (Optional) If a file has headers or not. Used to auto increment the currentRow variable.
+     * @returns {string|boolean} The path of the file if uploaded. If not, returns false
+    */
+    this.selectFile = (message,hasHeaders)=>{
+        if(!message)
+            message = "Please select a file";
         
-        this.read = function(rowNumber)
+        var upload=imns.Dialogs.browseForFileOpen(message);
+        if(upload)
         {
-                if(this.totalColumns==null)
-                        this.readTotalColumns();
-                        
-                if(!rowNumber)
-                        this.currentRow++,rowNumber=this.currentRow;
-                
-                code="CODE:SET !DATASOURCE "+this.folder+"\\"+this.file+"\n";
-                code+="SET !DATASOURCE_LINE "+rowNumber+"\n";
-        
-                for(var loop=1;loop<=this.totalColumns;loop++)
-                        code+="ADD !EXTRACT {{!COL"+(loop)+"}}\n";
-                
-                if(iimPlay(code)<0)
-                {
-                        iimPlayCode("WAIT SECONDS=0");
-                                  this.totalColumns=null;
-                        return false;
-                }
-                
-                for(loop=1;loop<=this.totalColumns;loop++)
-                this.values[loop-1]=iimGetLastExtract(loop);
-                
-                return this.values.slice(0);
-        };
-        
-        this.getContents = function()
-        {
-                var message="";
-                for(var row=1;1;row++)
-                {
-                        for(var col=1;1;col++)
-                        {
-                                var read=iimPlayCode("SET !DATASOURCE "+this.folder+"\\"+this.file+"\nSET !DATASOURCE_LINE "+row+"\nSET !EXTRACT {{!COL"+col+"}}");
-                                if(read==-953)
-                                {
-                                        iimPlayCode("WAIT SECONDS=0");
-                                        message=message.slice(0,-2);
-                                        message+="<BR>";
-                                        break;
-                                }
-                                if(read==-951)
-                                {
-                                        iimPlayCode("WAIT SECONDS=0");
-                                        return message;
-                                }
-                                message+=iimGetLastExtract()+", ";
-                        }
-                }
-        };
-        
-        this.readFull = function()
-        {
-          iimPlayCode("TAB OPEN\nTAB T=2\nURL GOTO=file://"+this.folder.replace(/ /g,"<SP>")+"\\"+this.file.replace(/ /g,"<SP>")+"\nTAG POS=1 TYPE=PRE ATTR=* EXTRACT=TXT\nTAB CLOSE");
-          this.values=iimGetLastExtract();
-          return iimGetLastExtract();
-        };
-        
-        this.write = function(data)
-        {
-                code="CODE:";
-                for(fileWriteloop=0;fileWriteloop<data.length;fileWriteloop++)
-                        code+="ADD !EXTRACT \""+data[fileWriteloop]+"\"\n";
-                code+="SAVEAS TYPE=EXTRACT FOLDER="+this.folder.replace(/ /g,"<SP>")+" FILE="+this.file+"\n";
+            var breakpoint=upload.path.lastIndexOf(this.separator);
+            this.folder = upload.path.substring(0,breakpoint).addSP();
+            this.file = upload.path.substring(breakpoint+1).addSP();
 
-                if(iimPlay(code)<0)
-                        return false;
-                return true;
-        };
-        
-		this.writeRaw = function(data)
-		{
-			if(this.folder=="{{!FOLDER_DATASOURCE}}")
-			{
-				iimPlayCode("SET !EXTRACT {{!FOLDER_DATASOURCE}}");
-				code = iimGetLastExtract();
-			}
-			else
-				code=this.folder;
-			try
-			{
-				var outputFile = imns.FIO.openNode(code+"\\"+this.file);
-                imns.FIO.appendTextFile(outputFile, data);
-				return true;
-			}
-			catch(e)
-			{
-				return false;
-			}
-		};        
-        
-        this.remove = function()
-        {
-                if(iimPlayCode("SET !DATASOURCE "+this.folder+"\\"+this.file)<0)
-                return false;
-                if(iimPlayCode("FILEDELETE NAME="+this.folder+"\\"+this.file)<0)
-                return false;
-                
-                return true;
-        };
-        
-        this.set = function(folder,file)
-        {
-                if(folder)
-                this.folder=folder.replace(/ /g,"<SP>");
-                if(file)
-                this.file=file.replace(/ /g,"<SP>");
-        };
-        
-        this.screenshot = function()
-        {
-                iimPlayCode("SAVEAS TYPE=JPEG FOLDER="+this.folder+" FILE="+this.file);
-        };
-  
-        this.getPreferences = function()
-        {
-          iimPlayCode("TAB OPEN\n TAB T=2\nURL GOTO=file://"+this.folder+"\\"+this.file+"\nTAG POS=1 TYPE=PRE ATTR=* EXTRACT=TXT\nTAB CLOSE");
-          return JSON.parse(iimGetLastExtract());
-        };
-                                                                                 
-         this.save = function()
-         {
-                 this.write(this.values);
-                 this.values=[];
-         };
-                
-         this.push= function(val)
-         {
-                 this.values.push(val);
-         };                                                                                 
-                  
-        this.loadPreferences = function()
-        {
-                const XMLHttpRequest = Components.Constructor("@mozilla.org/xmlextras/xmlhttprequest;1");
-                        var fetchData= XMLHttpRequest();
-                        fetchData.open('GET',"file://"+this.folder+"/"+this.file,false);
-                        fetchData.send();
-                        this.values = JSON.parse(fetchData.response);
-        };
-        
-        this.loadList = function()
-        {
-          const XMLHttpRequest = Components.Constructor("@mozilla.org/xmlextras/xmlhttprequest;1");
-          var fetchData= XMLHttpRequest();
-          fetchData.open('GET',"file://"+this.folder.replace(/<SP>/g," ")+"\\"+this.file.replace(/<SP>/g," "),false);
-          fetchData.send();
-          this.values=fetchData.response.replace(/"/g,"").split("\r\n");          
-        };
-                  
-        this.screenshotElement = function(type,attr,pos)
-        {
-           if(!pos)
-              pos=1;
-           iimPlayCode("ONDOWNLOAD FOLDER="+this.folder.replace(/ /g,"<SP>")+" FILE="+this.file.replace(/ /g,"<SP>")+"\nTAG POS="+pos+" TYPE="+type+" ATTR="+attr+" CONTENT=EVENT:SAVE_ELEMENT_SCREENSHOT");
-        };
-        
-		this.run = function()
-		{
-			this.temp = Components.classes["@mozilla.org/file/local;1"]
-								 .createInstance(Components.interfaces.nsILocalFile);
-								 
-			if(this.folder=="{{!FOLDER_DATASOURCE}}")
-			{
-				iimPlayCode("SET !EXTRACT {{!FOLDER_DATASOURCE}}");
-				code = iimGetLastExtract();
-			}
-			else
-				code=this.folder;								 
-			
-			this.temp.initWithPath(code+"\\"+this.file);
-			this.temp.launch();			
-         };
-}
+            if(hasHeaders)
+                this.currentRow=2;
+
+            return upload.path;
+        }
+        return false;
+    };
+};
